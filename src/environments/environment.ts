c8861cf444e36b09d8bb4f6fa40d0dbfa620f6e6
hjeong2020 <64095720+hjeong2020@users.noreply.github.com>
2020-04-28 14:38:18 -0500

    added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 const HOSTNAME: string = "localhost";
 const PORT_NUMBER: number = 8080;
+
+
 const APPLICATION_NAME: string = '/Flight_Manager';
 export const environment = {
   production: false,
   travelerAPIUrl: 'http://' + HOSTNAME + ':' + PORT_NUMBER + APPLICATION_NAME + '/Traveller',
-  adminAPIUrl: 'http://' + HOSTNAME + ':' + PORT_NUMBER + APPLICATION_NAME + '/Admin'
+  adminAPIUrl: 'http://' + HOSTNAME + ':' + PORT_NUMBER + APPLICATION_NAME + '/Admin',
+  travelerSearchAPIUrl: 'http://' + HOSTNAME + ':' + PORT_NUMBER + APPLICATION_NAME + '/Search'
 
 };
